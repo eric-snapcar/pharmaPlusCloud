7ac8f707f7e882ac4f1987398f5f36095903753b
Eric Hth <eric@snapcar.com>
2018-05-14 13:49:52 +0200

    index.js exports addMessage testHello

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,4 @@
 const functions = require('firebase-functions');
-
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -8,17 +7,12 @@ const functions = require('firebase-functions');
 // });
 const admin = require('firebase-admin');
 admin.initializeApp();
-
-
-
-// Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path /messages/:pushId/original
 exports.addMessage = functions.https.onRequest((req, res) => {
-  // Grab the text parameter.
-  const original = req.query.text;
-  // Push the new message into the Realtime Database using the Firebase Admin SDK.
-  return admin.database().ref('/messages').push({original: original}).then((snapshot) => {
-    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+  const text = req.query.text;
+  return admin.database().ref('/messages').push({test: text}).then((snapshot) => {
     return res.redirect(303, snapshot.ref.toString());
   });
+});
+exports.testHello = functions.https.onRequest((request, response) => {
+    response.send("Hello from Firebase!");
 });
