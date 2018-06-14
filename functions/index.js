3770ffa510f4f1598a77efb1fbc49c8d3cb34e1a
Eric Hth <eric@snapcar.com>
2018-06-15 00:22:01 +0200

    index.js addMessage onCall() data context

--- functions/index.js
+++ functions/index.js
@@ -1,38 +1,11 @@
 const functions = require('firebase-functions');
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
 const admin = require('firebase-admin');
 admin.initializeApp();
-exports.addMessage = functions.https.onRequest((req, res) => {
-  const text = req.query.text;
-  return admin.database().ref('/messages').push({test: text}).then((snapshot) => {
-    return res.redirect(303, snapshot.ref.toString());
-  });
-});
-exports.testHello = functions.https.onRequest((request, response) => {
-    response.send("Test Hello");
-});
-exports.addMessageBis = functions.https.onCall((data, context) => {
+exports.addMessage = functions.https.onCall((data, context) => {
   const text = data.text;
-  // Authentication / user information is automatically added to the request.
-  /*
-      const uid = context.auth.uid;
-      const name = context.auth.token.name || null;
-      const picture = context.auth.token.picture || null;
-      const email = context.auth.token.email || null;
-  */
-  const sanitizedMessage = text; // Sanitize the message.
   return admin.database().ref('/messages').push({
-    text: sanitizedMessage,
+    text: text,
   }).then(() => {
-    return { text: sanitizedMessage, test:"TEST" };
+    return { text: text};
   })
-
-
 });
-
-// onRequest req res return
